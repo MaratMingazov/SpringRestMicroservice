0c9eefd79a2f30d5e45f403591fcd7bf8811fccd
MaratMingazov <maratmingazovr@gmail.com>
2021-05-07 19:34:20 +0300

    Initial commit

---
+++ src/main/java/com/spring/rest/microservice/springRestMicroservice/SpringRestMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rest.microservice.springRestMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/rest/microservice/springRestMicroservice/SpringRestMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rest.microservice.springRestMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
